4ecaa8ba1bc1e69474c8695e9f43cf513ac087b0
OG Michael <6el0zu61979@mail.ru>
2024-07-06 19:08:02 +0700

    Initial commit

--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "Russian");
+    float hp;
+    float magicResistance;
+    float fireballDmg;
+
+
+        wcout << L"Введите текущий уровень здоровья орка: ";
+        cin >> hp;
+        while(true) {
+            if (hp <= 0) {
+                wcout << L"Неверный ввод. Уровень здоровья не может быть 0" << endl;
+                continue;
+            }
+
+            wcout << L"Введите уровень сопротивления магии: ";
+            cin >> magicResistance;
+
+            if (magicResistance < 0) {
+                wcout << L"Неверный ввод. Уровень сопротивления магии не может быть 0" << endl;
+                continue;
+            }
+
+            wcout << L"Введите урон огненного шара: ";
+            cin >> fireballDmg;
+
+            if (fireballDmg < 0) {
+                wcout << L"Неверный ввод. Урон огненного шара не может быть 0" << endl;
+                continue;
+            }
+        }
+        float adjustedFireballDmg;
 
 
 
