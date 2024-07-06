75f72d17837b2e1a836529cf519954cea736dae6
OG Michael <6el0zu61979@mail.ru>
2024-07-06 19:54:29 +0700

    HZ commit

--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -7,35 +7,35 @@ int main() {
     float magicResistance;
     float fireballDmg;
 
-        while(true) {
-            wcout << L"Введите текущий уровень здоровья орка: ";
-            cin >> hp;
 
-            if (hp < 0) {
-                wcout << L"Неверный ввод. Уровень здоровья не может быть ниже 0" << endl;
-                continue;
-            }
+    wcout << L"Введите текущий уровень здоровья орка (не более 100): ";
+    cin >> hp;
+    if (hp < 0 || hp > 100) {
+        wcout << L"Неверный ввод" << endl;
+    }
+    hp /= 100;
 
-            wcout << L"Введите уровень сопротивления магии: ";
-            cin >> magicResistance;
+    wcout << L"Введите уровень сопротивления магии (не более 100): ";
+    cin >> magicResistance;
+    if (magicResistance < 0 || magicResistance > 100) {
+        wcout << L"Неверный ввод!" << endl;
+    }
+    magicResistance /= 100;
 
-            if (magicResistance < 0) {
-                wcout << L"Неверный ввод. Уровень сопротивления магии не может быть 0" << endl;
-                continue;
-            }
+    wcout << L"Введите урон огненного шара (не более 100): ";
+    cin >> fireballDmg;
+    if (fireballDmg < 0 || fireballDmg > 100) {
+        wcout << L"Неверный ввод" << endl;
+    }
+    fireballDmg /= 100;
 
-            wcout << L"Введите урон огненного шара: ";
-            cin >> fireballDmg;
 
-            if (fireballDmg < 0) {
-                wcout << L"Неверный ввод. Урон огненного шара не может быть 0" << endl;
-                continue;
-            }
-
-            if(hp <= 0){
-                wcout << L"Орк погибает. GG";
-                break;
-            }
+    while(true){
+        if(hp <= 0){
+            wcout << L"Ну всё, GG" << endl;
+            break;
         }
-        float adjustedFireballDmg;
+        hp -= fireballDmg - magicResistance;
+        wcout << L"HP орка падает на " << fireballDmg - magicResistance << L" единиц здоровья. Новый уровень здоровья\n: " << hp * 100 << endl;
+    }
 }
