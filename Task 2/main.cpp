5300d7ee61b62c47e2fab682becfa54921eadcf3
OG Michael <6el0zu61979@mail.ru>
2024-07-11 12:41:28 +0700

    NOT DONE

--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -7,29 +7,29 @@ int main() {
     float magicResistance;
     float fireballDmg;
 
+    {
+        wcout << L"Введите текущий уровень здоровья орка (не более 100): ";
+        cin >> hp;
+        if (hp < 0 || hp > 100) {
+            wcout << L"Неверный ввод" << endl;
+        }
 
-    wcout << L"Введите текущий уровень здоровья орка (не более 100): ";
-    cin >> hp;
-    if (hp < 0 || hp > 100) {
-        wcout << L"Неверный ввод" << endl;
+        hp /= 100;
+        wcout << L"Введите уровень сопротивления магии (не более 100): ";
+        cin >> magicResistance;
+        if (magicResistance < 0 || magicResistance > 100) {
+            wcout << L"Неверный ввод!" << endl;
+        }
+        magicResistance /= 100;
+
+        wcout << L"Введите урон огненного шара (не более 100): ";
+        cin >> fireballDmg;
+        if (fireballDmg < 0 || fireballDmg > 100) {
+            wcout << L"Неверный ввод" << endl;
+        }
+        fireballDmg /= 100;
+
     }
-    hp /= 100;
-
-    wcout << L"Введите уровень сопротивления магии (не более 100): ";
-    cin >> magicResistance;
-    if (magicResistance < 0 || magicResistance > 100) {
-        wcout << L"Неверный ввод!" << endl;
-    }
-    magicResistance /= 100;
-
-    wcout << L"Введите урон огненного шара (не более 100): ";
-    cin >> fireballDmg;
-    if (fireballDmg < 0 || fireballDmg > 100) {
-        wcout << L"Неверный ввод" << endl;
-    }
-    fireballDmg /= 100;
-
-
     while(true){
         if(hp <= 0){
             wcout << L"Ну всё, GG" << endl;
