067a3c53b7100f27943fb4596816ca892cb29119
OG Michael <6el0zu61979@mail.ru>
2024-08-05 03:05:36 +0700

    Ctrl Commit

--- Task_3/cmake-build-debug/cmake_install.cmake
+++ Task_3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/User/Desktop/CLion Projects/8.6 Tasks/Task_3
+# Install script for directory: C:/Users/User/Desktop/C++/8.6 Tasks/Task_3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/User/Desktop/CLion Projects/8.6 Tasks/Task_3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/User/Desktop/C++/8.6 Tasks/Task_3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Task_3/main.cpp
+++ Task_3/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0; 
+    setlocale(LC_ALL, "Russian");
+
+    float x, y, z;
+    wcout << L"Введите размеры бруска в формате Высота Длинна Ширина:\t";
+    cin >> x >> y >> z;
+
+    if(x <= 0 || y <= 0 || z <= 0){
+        wcout << L"Размеры бруска не могут быть отрицательными или равными нулю.\n";
+        return 0;
+    }
+
+    int length = y / (float) 5;
+    int width = z / (float) 5;
+    int height = x / (float) 5;
+
+    int finalQuantity = (width * length) * height;
+
+    string line = "Из бруса размерностью " + to_string(x) + " X " + to_string(y) + " X " + to_string(z) + " можно составить " + to_string(finalQuantity / 27) + " наборов ";
+
+    cout << line << endl;
+
 }
