7affc2ef7db2d5a9b497b59c45bf99a818e8d02c
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-05 21:14:30 +0700

    Task 4 improvements

--- Task_4/main.cpp
+++ Task_4/main.cpp
@@ -4,23 +4,21 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
-
-    uint fileSize;
-    uint connectionSpeed;
-    uint progress = 0;
+    
     uint percentage;
+    uint fileSize;
+    uint progress = 0;
+    uint connectionSpeed;
     wcout << L"Введите размер вашего файла: " << endl;
     cin >> fileSize;
     wcout << L"Введите скорость вашего соединения" << endl;
     cin >> connectionSpeed;
-
-
-    for (;progress < fileSize;) {
+    
+    for (; progress < fileSize;) {
         progress += connectionSpeed;
         percentage = (progress * 100) / fileSize;
         if(percentage > 100) percentage = 100;
         if(progress > fileSize) progress = fileSize;
         wcout << L"Прогресс скачивания:\t" << progress << L" из " << fileSize << " (" << percentage << " %) " << endl;
-
     }
 }
